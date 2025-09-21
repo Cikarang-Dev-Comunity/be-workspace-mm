d7b61ff69cd3f9714e833df750fbaa4fc545d4ca
acepMunawar <acepmunawar96@gmail.com>
2025-09-22 03:23:00 +0700

    feat(project): new

---
+++ src/main/java/com/cd/be_workspace_mm/BeWorkspaceMmApplication.java
@@ -0,0 +1,13 @@
+package com.cd.be_workspace_mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWorkspaceMmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeWorkspaceMmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cd/be_workspace_mm/BeWorkspaceMmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.be_workspace_mm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWorkspaceMmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
